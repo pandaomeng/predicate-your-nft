9927bf08c3d520f7ecdf438b8c09aff21a4b9277
pandaomeng <pdaomeng@gmail.com>
2021-09-08 05:46:29 +0800

    分页功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Predicate from './components/Predicate'
+// import Title from './components/Title'
 
 const App: React.FC = () => (
-  <div className="App">
+  <div className="App" style={{ marginTop: 50 }}>
+    {/* <Title /> */}
     <Predicate />
   </div>
 )
--- src/components/Predicate.tsx
+++ src/components/Predicate.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import Web3 from 'web3'
-import { AbiItem } from 'web3-utils'
 import _ from 'lodash'
+import { Pagination } from 'antd'
 import './Predicate.css'
+import WowLootABI from './WowLootABI'
 
 interface NFTMetadata {
   tokenId: string
@@ -12,29 +13,22 @@ interface NFTMetadata {
 }
 
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
+const contract = new web3.eth.Contract(WowLootABI, '0xa39fb2c494b457593f9cbbef4a02f799330ddfd8')
 
-const FACTORY_ABI: AbiItem[] = [
-  {
-    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
-    name: 'tokenURI',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-]
-
-const PAGE_SIZE = 50
+const PAGE_SIZE = 100
 
 const Predicate: React.FC = () => {
-  const [page, setPage] = useState(0)
+  const [currentPage, setCurrentPage] = useState(0)
+  const [total, setTotal] = useState(10000)
+  const [loading, setLoading] = useState(false)
   // const images =
   const [nfts, setNfts] = useState<NFTMetadata[]>([])
 
-  const setNftThrottle = useMemo(() => _.throttle(setNfts, 500), [setNfts])
+  const setNftThrottle = useMemo(() => _.throttle(setNfts, 50), [setNfts])
 
-  const query = async () => {
-    const contract = new web3.eth.Contract(FACTORY_ABI, '0xa39fb2c494b457593f9cbbef4a02f799330ddfd8')
-
+  const query = async (page: number) => {
+    // setLoading(true)
+    setCurrentPage(page)
     const batch = new web3.BatchRequest()
 
     // 使用一个数组存结果
@@ -68,33 +62,56 @@ const Predicate: React.FC = () => {
     }
   }
 
+  // const queryTotalSupply = async () => {
+  //   const totalSupply = await contract.methods.totalSupply().call()
+  //   console.log(totalSupply)
+  //   setTotal(totalSupply)
+  // }
+
   useEffect(() => {
-    query()
+    // queryTotalSupply()
+    query(0)
   }, [])
 
-  console.log('nfts: ', nfts)
+  const onPageChange = (newPage: number) => {
+    setNfts([])
+    setLoading(true)
+    query(newPage)
+  }
 
   return (
-    <div style={{ display: 'flex', width: '100%', flexWrap: 'wrap' }}>
-      {nfts.map(each => (
-        <div key={each.tokenId} style={{ width: 300, padding: 20, display: 'flex', flexDirection: 'column' }}>
-          <div>
-            <img
-              key={each.tokenId}
-              alt="nft img"
-              style={{
-                display: 'block',
-                width: 300,
-                height: 300,
-              }}
-              id={each.tokenId}
-              src={each.image}
-            />
+    <div style={{ margin: 'auto' }}>
+      <Pagination
+        defaultCurrent={1}
+        total={total}
+        pageSize={PAGE_SIZE}
+        style={{ textAlign: 'center' }}
+        showSizeChanger={false}
+        onChange={onPageChange}
+        showQuickJumper
+      />
+
+      <div className="card-container">
+        {nfts.map(each => (
+          <div key={each.tokenId} style={{ width: 340, padding: 20, display: 'flex', flexDirection: 'column' }}>
+            <div>
+              <img
+                key={each.tokenId}
+                alt="nft img"
+                style={{
+                  display: 'block',
+                  width: 300,
+                  height: 300,
+                }}
+                id={each.tokenId}
+                src={each.image}
+              />
+            </div>
+            <div style={{ fontSize: '20px', padding: '5px 0 5px' }}>{each.name}</div>
+            <div className="description">{each.description}</div>
           </div>
-          <div style={{ fontSize: '20px', padding: '5px 0 5px' }}>{each.name}</div>
-          <div className="description">description: {each.description}</div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
