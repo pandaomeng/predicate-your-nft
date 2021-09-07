0039d33a01707d4bd1bec5c1d72995048c336399
pandaomeng <pdaomeng@gmail.com>
2021-09-07 18:22:48 +0800

    add prettier

--- src/components/Predicate.tsx
+++ src/components/Predicate.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+
+const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+
+const FACTORY_ABI: AbiItem[] = [
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
+    name: 'tokenURI',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
 
 export default function Predicate() {
-  return (
-    <div>
-      this is predicate page
-    </div>
-  )
+  useEffect(() => {
+    var contract = new web3.eth.Contract(
+      FACTORY_ABI,
+      '0xa39fb2c494b457593f9cbbef4a02f799330ddfd8'
+    );
+
+    contract.methods.tokenURI('');
+
+    return () => {};
+  }, []);
+
+  return <div>this is predicate page</div>;
 }
